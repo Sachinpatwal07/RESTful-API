9765c21d00c33148865699464876350971284773
sachinpatwal07 <sachinpatwal078@gmail.com>
2021-05-29 16:45:50 +0530

    Post HTTP verb added

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ app.use(express.static("public"));
 mongoose.connect("mongodb://localhost:27017/wikiDB", {  useNewUrlParser: true,  useUnifiedTopology: true });
 
 
-
 const articleSchema = {
 
 title:String,
 content:String
 
 }
+
 
 const Article= mongoose.model("Article",articleSchema);
 
@@ -37,6 +37,46 @@ app.get("/articles",(req,res)=>{
 
 
 })
+
+
+
+app.post("/articles",(req,res)=>{
+
+
+  const newArticle= new Article({
+
+
+  title:req.body.title,
+  content:req.body.content
+
+  })
+
+  newArticle.save((err)=>{
+if(!err)
+res.send("Successfully added a new article");
+else
+res.send(err);
+
+  });
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
