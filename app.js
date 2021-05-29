1ddda55335d6015343073d70940d9e9843a6d547
sachinpatwal07 <sachinpatwal078@gmail.com>
2021-05-29 22:28:38 +0530

    Final commit

--- app.js
+++ app.js
@@ -7,92 +7,154 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({  extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", {  useNewUrlParser: true,  useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 
 const articleSchema = {
 
-title:String,
-content:String
+  title: String,
+  content: String
 
 }
-const Article= mongoose.model("Article",articleSchema);
+const Article = mongoose.model("Article", articleSchema);
 
+//<--------------------------------------------Requests Targetting all Articles--------------------------------------------------------->
 
+app.route("/articles")
+  .get((req, res) => {
 
-// <----------------------GET REQUEST VERB------------------------->
-app.get("/articles",(req,res)=>{
+    Article.find({}, (err, foundArticles) => {
 
- Article.find({},(err,foundArticles)=>{
+      if (!err)
+        res.send(foundArticles);
+      else
+        res.send(err);
 
- if(!err)
- res.send(foundArticles);
- else
- res.send(err);
+    })
 
- })
-
-
-})
-
-
-// <----------------------POST REQUEST VERB------------------------->
-
-app.post("/articles",(req,res)=>{
-
-
-  const newArticle= new Article({
-
-
-  title:req.body.title,
-  content:req.body.content
 
   })
 
-  newArticle.save((err)=>{
-if(!err)
-res.send("Successfully added a new article");
-else
-res.send(err);
+  .post((req, res) => {
 
-  });
+    const newArticle = new Article({
 
+      title: req.body.title,
+      content: req.body.content
 
-})
+    })
 
+    newArticle.save((err) => {
+      if (!err)
+        res.send("Successfully added a new article");
+      else
+        res.send(err);
 
-
-// <----------------------DELETE REQUEST VERB------------------------->
-
-app.delete("/articles",(req,res)=>{
-
-Article.deleteMany((err)=>{
-
-if(!err)
-console.log("Successfully Deleted All the Articles");
-else
-console.log(err);
+    });
 
 
-})
+  })
+
+  .delete((req, res) => {
+
+    Article.deleteMany((err) => {
+
+      if (!err)
+        console.log("Successfully Deleted All the Articles");
+      else
+        console.log(err);
 
 
+    })
 
-})
-
-
-
+  })
 
 
+//<--------------------------------------------Requests Targetting specified Articles--------------------------------------------------------->
 
 
+app.route("/articles/:articleTitle")
+
+  .get((req, res) => {
+
+    const articleTitle = req.params.articleTitle;
+
+    Article.findOne({
+      title: articleTitle
+    }, (err, foundArticle) => {
+
+      if (!err) {
+
+        if (foundArticle)
+          res.send(foundArticle);
+        else
+          res.send("Item not found in articles");
+
+      } else
+        res.send(err);
+    })
+  })
+  .put((req, res) => {
 
 
+    Article.update({
+        title: req.params.articleTitle
+      }, {
+        title: req.body.title,
+        content: req.body.content
+      }, {
+        overwrite: true
+      },
+      (err) => {
 
+        if (!err)
+          res.send("Iten updated Successfully");
+        else
+          res.send(err);
+
+
+      })
+
+  })
+  .patch((req, res) => {
+
+    Article.update({
+      title: req.params.articleTitle
+    }, {
+      $set: req.body
+    }, (err) => {
+      if (!err)
+        res.send("Selected item updated successfully");
+      else
+        res.send(err);
+    })
+
+
+  })
+  .delete((req, res) => {
+
+    Article.deleteOne({
+      title: req.params.articleTitle
+    }, (err) => {
+
+      if (!err)
+        res.send("Selected item updated successfully");
+      else
+        cres.send(err);
+
+
+    })
+
+  })
 
 
 
